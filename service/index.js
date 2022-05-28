fc6f0999e7cbb01de10c5a7ba25d7cd389cb4854
957525929@qq.com <957525929@qq.com>
2022-05-28 23:29:01 +0800

    init

---
+++ service/index.js
@@ -0,0 +1,30 @@
+const BASE_URL = "https://coderwhy-music.vercel.app/"
+
+class JHERequest {
+  request(url, method, params) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: BASE_URL + url,
+        method: method,
+        data: params,
+        success: function(res) {
+          console.log('res',res)
+          resolve(res.data)
+        },
+        fail: reject
+      })
+    })
+  }
+
+  get(url, params) {
+    return this.request(url, "GET", params)
+  }
+
+  post(url, data) {
+    return this.request(url, "POST", data)
+  }
+}
+
+const jheRequest = new JHERequest()
+
+export default jheRequest
